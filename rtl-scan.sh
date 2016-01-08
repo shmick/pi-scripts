ab765207ef1e7aba53d2773fcff90fe09285ca4b
Steve M <shmick@users.noreply.github.com>
2016-01-08 11:59:04 -0500

    Update rtl-scan.sh

--- rtl-scan.sh
+++ rtl-scan.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+rtlcheck() {
+# Exit if an RTL device isn't found
+RTLDEVICE="$(cat  /sys/bus/usb/devices/*/product | grep RTL)"
+if [ "$?" -ne 0 ]
+then
+exit $?
+fi
+}
+
+rtlcheck
+
 RTLP="/usr/local/bin/rtl_power"
 
+# Senn G Band
 FREQ_START="566M"
 FREQ_END="608M"
+
+# MiPro 
+#FREQ_START="620M"
+#FREQ_END="644M"
+
 STEP="10k"
-GAIN="50"
+GAIN="35"
 INTERVAL="5"
-MINUTES="30m"
 
 filenum () {
-LASTFILE=$(ls $HOME/scans | tail -1 | grep "scan-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].log")
+LASTFILE=$(ls $HOME/scans | tail -1 | grep "scan-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].log") 
 
 if [ "$?" -ge 1 ]
 then
@@ -24,6 +40,7 @@ LOG="${HOME}/scans/scan-${FILENUM}.log"
 
 while true
 do
+rtlcheck
 filenum
-$RTLP -f $FREQ_START:$FREQ_END:$STEP -g $GAIN -i $INTERVAL -e $MINUTES $LOG
+$RTLP -f $FREQ_START:$FREQ_END:$STEP -g $GAIN -i $INTERVAL $LOG
 done
