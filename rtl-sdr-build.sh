8c82322bf78a48de2b0d6691a351ad1bbb6d9e7c
Steve M <shmick@users.noreply.github.com>
2016-01-08 21:04:16 -0500

    Update rtl-sdr-build.sh

--- rtl-sdr-build.sh
+++ rtl-sdr-build.sh
@@ -5,7 +5,8 @@ sudo apt-get install -y libusb-1.0-0-dev pkg-config ca-certificates git-core cma
 sudo apt-get clean
 
 echo 'blacklist dvb_usb_rtl28xxu' | sudo tee /etc/modprobe.d/raspi-blacklist.conf > /dev/null
-git clone git://git.osmocom.org/rtl-sdr.git 
+#git clone git://git.osmocom.org/rtl-sdr.git 
+git clone https://github.com/keenerd/rtl-sdr rtl-sdr
 mkdir rtl-sdr/build 
 cd rtl-sdr/build 
 cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON 
