ca8efb62ce2c84c27add7c2ab308274ddac5c595
Steve M <shmick@users.noreply.github.com>
2016-01-20 15:47:44 -0500

    Update web-date.sh

--- web-date.sh
+++ web-date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Add FQDNs or IPs in here
-HOSTS="www.nrc-cnrc.gc.ca google.com amazon.com www.nrc-cnrc.gc.ca"
+HOSTS="www.nrc-cnrc.gc.ca google.com amazon.com"
 for host in $HOSTS
 do 
 DATE="$(curl -m 3 -Is http://${host} | grep "^Date:" | sed 's/^Date: //' | grep " 20[0-9][0-9] ")"
