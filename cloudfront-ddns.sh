60ec33d2726377a53c91ce49ae7a9cb812f624f8
Steve M <shmick@users.noreply.github.com>
2016-02-24 19:55:53 -0500

    Update cloudfront-ddns.sh

--- cloudfront-ddns.sh
+++ cloudfront-ddns.sh
@@ -1,59 +1,70 @@
 #!/bin/bash
 
-# Requires curl and jq
+# Requires: curl and jq
 # apt-get install -y curl jq
+
+# This script will attempt to update the host entry
+# ddns.example.com on CloudFlare
+#
+# You need to first make sure that you have an A record
+# for ddns.example.com CloudFlare
+#
+# Change the below to your own info
 
 # This will be the primary domain to check against
 pridomain="example.com"
 
-# Get our current external IP
-myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+# This the full list of domains to check against
+mydomains="$pridomain example.net example.org"
+
+# Your CloudFlare email address
+cf_email="you@example.com"
+
+# Your CloudFlare Global API Key
+cf_tkn="12345678901234567890"
+
+# Your ddns hostname, by default we use "ddns"
+ddnshost="ddns"
+
+# END OF CONFIG OPTIONS
 
 # Determine the CloudFlare DNS server for the primary domain
 mycfdns=$(dig +short ns "$pridomain" @resolver1.opendns.com. | grep -m1 ns.cloudflare.com)
 if [ "$?" -ne "0" ]
 then
-echo "$pridomain is not using CloudFlare for DNS"
+echo "$pridomain does not appear to be using using CloudFlare for DNS"
 exit 1
 fi
 
 # Query the CloudFlare DNS for the primary domain
 curip=$(dig +short "$pridomain" @$mycfdns)
-
-# Only attempt to update the DNS enrty if the IP has changed or
-# the script has been run as "cloudfront-ddns.sh force"
+# Get our current external IP
+myip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+# 
 if [ "$myip" != "$curip" ] || [ "$1" = "force" ]
 then
-
 cf_api="https://www.cloudflare.com/api_json.html"
-cf_email="email.address"
-cf_tkn="YourCloudFrontToken"
-# List of domains, separated by a space
-mydomains="example.com example.net"
+    for i in $mydomains
+    do
+    # Get the record ID of the $ddnshost A record
+    domain="$i"
+    rec_id=$(curl -s "$cf_api" \
+    -d "a=rec_load_all" \
+    -d "tkn=$cf_tkn" \
+    -d "email=$cf_email" \
+    -d "z=$domain" \
+    | jq --arg ddns "$ddnshost.$domain" -r '.response.recs.objs[] | select(.type == "A" and .name == $ddns) | .rec_id')
 
-for i in $mydomains
-do
-# Get the record ID of root A record
-domain="$i"
-rec_id=$(curl -s "$cf_api" \
--d "a=rec_load_all" \
--d "tkn=$cf_tkn" \
--d "email=$cf_email" \
--d "z=$domain" \
-| jq --arg mydom "$domain" -r '.response.recs.objs[] | select(.type == "A" and .name == $mydom) | .rec_id')
-
-curl -w "%{http_code}\\n" -o /dev/null -sq "$cf_api" \
--d "a=rec_edit" \
--d "tkn=$cf_tkn" \
--d "id=$rec_id" \
--d "email=$cf_email" \
--d "z=$domain" \
--d "type=A" \
--d "name=@" \
--d "content=$myip" \
--d "service_mode=0" \
--d "ttl=120"
-
-done
-
+    curl -w "%{http_code}\\n" -o /dev/null -sq "$cf_api" \
+    -d "a=rec_edit" \
+    -d "tkn=$cf_tkn" \
+    -d "id=$rec_id" \
+    -d "email=$cf_email" \
+    -d "z=$domain" \
+    -d "type=A" \
+    -d "name=$ddnshost" \
+    -d "content=$myip" \
+    -d "service_mode=0" \
+    -d "ttl=120"
+    done
 fi
